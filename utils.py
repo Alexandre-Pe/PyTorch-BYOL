49004574ddcf5dbe3602cd4efa18f1cf3550bf6e
Alexandre-Pe <83660838+Alexandre-Pe@users.noreply.github.com>
2024-09-24 18:46:20 +0200

    Preprocess MPIIFaceGaze dataset

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 from shutil import copyfile
 
+VSC_DATA = os.getenv('VSC_DATA')
+VSC_SCRATCH = os.getenv('VSC_SCRATCH')
 
 def _create_model_training_folder(writer, files_to_same):
     model_checkpoints_folder = os.path.join(writer.log_dir, 'checkpoints')
@@ -8,3 +10,6 @@ def _create_model_training_folder(writer, files_to_same):
         os.makedirs(model_checkpoints_folder)
         for file in files_to_same:
             copyfile(file, os.path.join(model_checkpoints_folder, os.path.basename(file)))
+
+def eval_path(path):
+    return path.replace("VSC_DATA", VSC_DATA).replace("VSC_SCRATCH", VSC_SCRATCH)
